99e845414c0e8258406aa6c61cc8b14b9d9d3647
Andrea <tdgshowpay@gmail.com>
2025-10-29 21:48:48 +0100

    Primer commit

---
+++ Tekku/src/main/java/TFC/Tekku/TekkuApplication.java
@@ -0,0 +1,13 @@
+package TFC.Tekku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TekkuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TekkuApplication.class, args);
+	}
+
+}
---
+++ Tekku/src/test/java/TFC/Tekku/TekkuApplicationTests.java
@@ -0,0 +1,13 @@
+package TFC.Tekku;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TekkuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
